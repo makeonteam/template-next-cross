8e84c30884922737bc38526eaaa8393ec8583ea1
MakeOnTeam <makeonteam+develop@gmail.com>
2025-09-19 15:01:56 +0800

    add capacitor config.json package.json

---
+++ mobile/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "app.makeon.mobile",
+  appName: "makeon-mobile",
+  webDir: "../out",
+};
+
+export default config;
